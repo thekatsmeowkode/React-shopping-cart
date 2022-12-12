954b3ab6f25d9f7a3245475bf066ab4b201b3959
Kat <kmunson13@gmail.com>
2022-12-12 16:01:34 -0600

    Set up file structure and import images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes, Link} from 'react-router-dom'
+import React from 'react';
+import {Cart} from './Components/Cart'
+import {Shop} from './Components/Shop'
+import {Home} from './Components/Home'
+import {Checkout} from './Components/Checkout'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/shop'>Shop</Link></li>
+          <li><Link to='/cart'>Cart</Link></li>
+          <li><Link to='/checkout'>Checkout</Link></li>
+        </ul>
+      </nav>
+    <Routes>
+      <Route path = '/' element={<Home/>} />
+      <Route path = '/shop' element = {<Shop /> } />
+      <Route path = '/cart' element = {<Cart />} />
+      <Route path = '/checkout' element = {<Checkout/>}/>
+    </Routes>
+    </React.Fragment>
   );
 }
 
